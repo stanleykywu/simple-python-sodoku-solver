79897eacee64e4a8ee51e48f9d22d33a1a76b5c4
stanleykywu <stanleykywu@gmail.com>
2019-12-24 22:18:13 -0500

    clarified variable and function names

--- solver.py
+++ solver.py
@@ -9,8 +9,8 @@ board = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
          [0, 4, 9, 2, 0, 6, 0, 0, 7]]
 
 
-def solve(bo):
-    find = find_empty(bo)
+def complete_grid(brd):
+    find = find_empty(brd)
 
     if not find:
         return True
@@ -18,67 +18,67 @@ def solve(bo):
         row, col = find
 
     for i in range(1, 10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if check_valid(brd, i, (row, col)):
+            brd[row][col] = i
 
-            if solve(bo):
+            if complete_grid(brd):
                 return True
 
-            bo[row][col] = 0
+            brd[row][col] = 0
 
     return False
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
+def find_empty(brd):
+    for i in range(len(brd)):
+        for j in range(len(brd[0])):
             if board[i][j] == 0:
                 return i, j  # row, col
 
     return None
 
 
-def valid(bo, num, pos):
+def check_valid(brd, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check column
-    for j in range(len(bo)):
-        if bo[j][pos[1]] == num and pos[0] != j:
+    for j in range(len(brd)):
+        if brd[j][pos[1]] == num and pos[0] != j:
             return False
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
+    # Check brdx
+    brdx_x = pos[1] // 3
+    brdx_y = pos[0] // 3
 
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i, j) != pos:  # i != pos[1] and j != pos[0]
+    for i in range(brdx_y * 3, brdx_y * 3 + 3):
+        for j in range(brdx_x * 3, brdx_x * 3 + 3):
+            if brd[i][j] == num and (i, j) != pos:  # i != pos[1] and j != pos[0]
                 return False
 
     return True
 
 
-def print_board(bo):
-    for i in range(len(bo)):
+def print_board(brd):
+    for i in range(len(brd)):
 
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - -")
 
-        for j in range(len(bo[0])):
+        for j in range(len(brd[0])):
 
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
             if j == 8:
-                print(bo[i][j])
+                print(brd[i][j])
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(brd[i][j]) + " ", end="")
 
 
 print_board(board)
-solve(board)
+complete_grid(board)
 print("Solved Version:")
 print_board(board)
