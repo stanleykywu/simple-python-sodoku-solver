53eb5565001761b1ca307895ad401f5666d61e54
stanleykywu <stanleykywu@gmail.com>
2019-12-25 19:06:35 -0500

    Further clarified code

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ board = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
 def complete_grid(brd):
     find = find_empty(brd)
 
-    if not find:
+    if not find:  # If no empty blocks exists the board has been completed
         return True
     else:
         row, col = find
@@ -22,12 +22,12 @@ def complete_grid(brd):
         if check_valid(brd, i, (row, col)):
             brd[row][col] = i
 
-            if complete_grid(brd):
+            if complete_grid(brd):  # Recursive call on the next empty grid
                 return True
 
-            brd[row][col] = 0
+            brd[row][col] = 0  # Backtrack if first attempted number does not work, recheck with next value
 
-    return False
+    return False  # Board cannot be solved
 
 
 # Finds the next empty square in a sudoku puzzle (marked by 0) and returns its position
@@ -52,12 +52,12 @@ def check_valid(brd, num, pos):
         if brd[j][pos[1]] == num and pos[0] != j:
             return False
 
-    # Check brdx
-    brdx_x = pos[1] // 3
-    brdx_y = pos[0] // 3
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
-    for i in range(brdx_y * 3, brdx_y * 3 + 3):
-        for j in range(brdx_x * 3, brdx_x * 3 + 3):
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
             if brd[i][j] == num and (i, j) != pos:  # i != pos[1] and j != pos[0]
                 return False
 
